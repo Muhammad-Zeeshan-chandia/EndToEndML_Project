9c94d09deddfedbb3d53c8fcbd54a1431d0ea434
Muhammad Zeeshan <m.zeeshanchandia1@gmail.com>
2025-07-03 15:53:24 +0500

    dataingestion

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,6 @@
 import sys
-import logging
+from src.logger import logging
 import traceback
-import logger  # ensure logging is configured
 
 def error_message_detail(error, error_detail=sys):
     _, _, exc_tb = error_detail.exc_info()
